f7c8b70ad4be38e613711efdef29b32eb7f7400d
Maël Minot <mminot@yseop.com>
2025-03-25 15:45:31 +0100

    refactor(bash-generator): main block; init function

--- bash/generator/create-bash-script.py
+++ bash/generator/create-bash-script.py
@@ -1,8 +1,12 @@
 #! /usr/bin/env python3
 
-output_lines = []
-current_indent_steps = 0
-indent_size = 4
+def init_writer():
+    global output_lines
+    output_lines = []
+    global current_indent_steps
+    current_indent_steps = 0
+    global indent_size
+    indent_size = 4
 
 def indent():
     global current_indent_steps
@@ -22,51 +26,56 @@ def print_output_lines():
     result = result.replace("\t", " " * indent_size)
     print(result)
 
-use_env = False
+if __name__ == "__main__":
+    init_writer()
 
-if use_env:
-    add_line("#! /usr/bin/env bash")
-else:
-    add_line("#! /bin/bash")
+    # Dummy-ish config. To be improved.
+    use_env = False
+    # End of dummy config.
 
-print("""
-if type greadlink &> /dev/null
-then
-  BASEDIR=$(dirname "$(greadlink -f -- "$0")")
-else
-  BASEDIR=$(dirname "$(readlink -f -- "$0")")
-fi
-readonly BASEDIR
+    if use_env:
+        add_line("#! /usr/bin/env bash")
+    else:
+        add_line("#! /bin/bash")
 
-# For internal use via the logging functions below.
-#
-# $1    String added between the program name and the message,
-#       typically to specify the log level.
-# $2    Printf-style format string.
-# $3…n  Arguments for printf.
-_f_log() {
-  local prog
-  prog=$(basename "$0"):
-  printf "%s %s${2}\n" "$prog" "$1" "${@:3}"
-}
+    print("""
+    if type greadlink &> /dev/null
+    then
+      BASEDIR=$(dirname "$(greadlink -f -- "$0")")
+    else
+      BASEDIR=$(dirname "$(readlink -f -- "$0")")
+    fi
+    readonly BASEDIR
 
-# $1    Printf-style format string.
-# $2…n  Arguments for printf.
-log() {
-  _f_log '   INFO  ' "$@"
-}
+    # For internal use via the logging functions below.
+    #
+    # $1    String added between the program name and the message,
+    #       typically to specify the log level.
+    # $2    Printf-style format string.
+    # $3…n  Arguments for printf.
+    _f_log() {
+      local prog
+      prog=$(basename "$0"):
+      printf "%s %s${2}\n" "$prog" "$1" "${@:3}"
+    }
 
-# $1    Printf-style format string.
-# $2…n  Arguments for printf.
-warn() {
-  _f_log 'WARNING  ' "$@" >&2
-}
+    # $1    Printf-style format string.
+    # $2…n  Arguments for printf.
+    log() {
+      _f_log '   INFO  ' "$@"
+    }
 
-# $1    Printf-style format string.
-# $2…n  Arguments for printf.
-err() {
-  _f_log '  ERROR  ' "$@" >&2
-}
+    # $1    Printf-style format string.
+    # $2…n  Arguments for printf.
+    warn() {
+      _f_log 'WARNING  ' "$@" >&2
+    }
 
-log 'Hello.'
-""")
+    # $1    Printf-style format string.
+    # $2…n  Arguments for printf.
+    err() {
+      _f_log '  ERROR  ' "$@" >&2
+    }
+
+    log 'Hello.'
+    """)
